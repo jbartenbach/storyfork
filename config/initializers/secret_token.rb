669b69ea830ed51b8e2e218c258a1744b39afbdb
Jeff Bartenbach <jbartenbach@gmail.com>
2012-05-15 11:31:21 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Storyfork::Application.config.secret_token = '8ca26f70ba460c012dbe13820dabb8e2c58a1af87ced9129a48f725c3ce95254725a4b57178a3f48a8994cb59d580f09889b70cd18ddb6b73b63fe2519244e5e'
